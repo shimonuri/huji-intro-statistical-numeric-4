4d51ac1a02a5b2c207813eca6b0bc55a8a4a4b74
shimonuri <54213090+shimonuri@users.noreply.github.com>
2022-12-22 12:48:55 +0200

    adding constants module

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 import model
 
 
-def main(temperature, stop_condition):
-    my_model = model.Model(temperature, stop_condition)
+def main(max_energy_level, number_of_particles, temperature, stop_condition):
+    my_model = model.Model(
+        number_of_particles=number_of_particles,
+        max_energy_level=max_energy_level,
+        temperature=temperature,
+        stop_condition=stop_condition,
+    )
     result = my_model.run()
 
 
 if __name__ == "__main__":
-    main(1, 0.2)
+    main(
+        max_energy_level=100, number_of_particles=100, temperature=1, stop_condition=100
+    )
