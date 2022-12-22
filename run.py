ea26dae7e8d2220ebfe0427d003de04b18c28bc4
shimonuri <54213090+shimonuri@users.noreply.github.com>
2022-12-22 14:12:26 +0200

    starting to implemented multiple_temperature run

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import model
+from matplotlib import pyplot as plt
+
 
 
 def main(number_of_particles, temperature, stop_condition):
@@ -8,8 +10,15 @@ def main(number_of_particles, temperature, stop_condition):
         stop_condition=stop_condition,
     )
     result = my_model.run()
-    print(result)
+    plt.plot(
+        list(result.particles.energy_level_to_occurrences.keys()),
+        list(result.particles.energy_level_to_occurrences.values()),
+    )
+    plt.show()
 
+def multiple_temperature_runs(number_of_particles, temperatures, stop_condition):
+    for temperature in temperatures:
+        main(number_of_particles, temperature, stop_condition)
 
 if __name__ == "__main__":
-    main(number_of_particles=100, temperature=0.2, stop_condition=1e-3)
+    #main(number_of_particles=100, temperature=15, stop_condition=1e-1)
