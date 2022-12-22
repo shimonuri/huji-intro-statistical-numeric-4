d731723dac77389318f63f28b29a45c325619910
AR1905 <assaf.reiner@gmail.com>
2022-12-22 14:11:29 +0200

    plot ground state expected value

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import model
+import matplotlib.pyplot as plt
 
+# update matplotlib params:
+plt.rcParams.update({'usetex': True})
+plt.rcParams.update({'font.size': 16})
 
 def main(max_energy_level, number_of_particles, temperature, stop_condition):
     my_model = model.Model(
@@ -9,6 +13,17 @@ def main(max_energy_level, number_of_particles, temperature, stop_condition):
         stop_condition=stop_condition,
     )
     result = my_model.run()
+
+
+def plot_ground_state_expected_value(temperature_range,
+                                     ground_state_expected_value,
+                                     number_of_particles,
+                                     ground_state_std_lst):
+    x = temperature_range
+    y = [expected_value / number_of_particles for expected_value in ground_state_expected_value]
+    plt.plot(x, y)
+    plt.xlabel(r'$T$')
+    plt.ylabel(r'$\left\langle N_{0}\right\rangle /N$')
 
 
 if __name__ == "__main__":
