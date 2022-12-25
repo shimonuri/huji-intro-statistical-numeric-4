0a92a5c1f816b98266486ea7227cb0caf60d6347
shimonuri <54213090+shimonuri@users.noreply.github.com>
2022-12-25 10:00:33 +0200

    Adding multiprocessing

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import calculations
 import click
 import logging
 import json
+import multiprocessing
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -18,12 +19,13 @@ plt.rcParams.update({"font.size": 16})
 @click.option("--particles", type=int, default=None)
 @click.option("--plot", is_flag=True, default=False)
 @click.option("--fast", is_flag=True, default=False)
-def main(path, particles, plot, fast):
+@click.option("--processes", "-p", type=int, default=1)
+def main(path, particles, plot, fast, processes):
     if not plot:
         if particles is None:
-            run_multiple_models(path, fast=fast)
+            run_multiple_models(path, fast=fast, processes=processes)
         else:
-            run_multiple_models(path, numbers_of_particles=[particles], fast=fast)
+            run_multiple_models(path, numbers_of_particles=[particles], fast=fast, processes=processes)
     else:
         with open(path, "rt") as file:
             data = json.load(file)
@@ -45,12 +47,12 @@ def plot_data(data):
         plot_specific_heat_capacity(
             temperature_range=number_of_particles_data["temperatures"],
             number_of_particles=int(float(number_of_particles)),
-            total_energy_std=number_of_particles_data["total_energy_stds"]
+            total_energy_std=number_of_particles_data["total_energy_stds"],
         )
     plt.show()
 
 
-def run_multiple_models(path, numbers_of_particles=None, fast=False):
+def run_multiple_models(path, numbers_of_particles=None, fast=False, processes=1):
     if numbers_of_particles is None:
         numbers_of_particles = [1e1, 1e2, 1e3, 1e4]
 
@@ -58,7 +60,7 @@ def run_multiple_models(path, numbers_of_particles=None, fast=False):
     for number_of_particles in numbers_of_particles:
         logging.info(f"Number of particles: {number_of_particles}")
         if fast:
-            temperatures = [0.2]
+            temperatures = [0.2, 1]
         else:
             temperatures = _get_temperatures(number_of_particles)
         (
@@ -66,7 +68,7 @@ def run_multiple_models(path, numbers_of_particles=None, fast=False):
             ground_state_stds,
             total_energy,
             total_energy_stds,
-        ) = multiple_temperature_runs(number_of_particles, temperatures)
+        ) = multiple_temperature_runs(number_of_particles, temperatures, processes)
         plot_ground_state_expected_value(
             temperature_range=temperatures,
             ground_state_expected_values=ground_state_expected_values,
@@ -86,28 +88,36 @@ def run_multiple_models(path, numbers_of_particles=None, fast=False):
             )
 
 
-def multiple_temperature_runs(number_of_particles, temperatures):
+def multiple_temperature_runs(number_of_particles, temperatures, processes):
     ground_state_expected_values = []
     ground_state_stds = []
     total_energy = []
     total_energy_stds = []
-    for temperature in temperatures:
-        logging.info(
-            f"Number of Particles: {number_of_particles}, Temperature: {temperature}"
+    with multiprocessing.Pool(processes=processes) as pool:
+        results = pool.starmap(
+            _run_model, [(number_of_particles, temperature) for temperature in temperatures]
         )
-        current_model = model.Model(
-            number_of_particles=number_of_particles,
-            temperature=temperature,
-            stop_condition=_get_stop_condition(temperature),
-        )
-        result = current_model.run()
+
+    for result in results:
         ground_state_expected_values.append(result.data.ground_level.expected_value)
         ground_state_stds.append(result.data.ground_level.std)
         total_energy_stds.append(result.data.total_energy_std)
         total_energy.append(result.data.total_energy_expected_value)
-    return ground_state_expected_values, ground_state_stds, total_energy, total_energy_stds
+    return (
+        ground_state_expected_values,
+        ground_state_stds,
+        total_energy,
+        total_energy_stds,
+    )
 
-
+def _run_model(number_of_particles, temperature):
+    current_model = model.Model(
+        number_of_particles=number_of_particles,
+        temperature=temperature,
+        stop_condition=_get_stop_condition(temperature),
+    )
+    result = current_model.run()
+    return result
 def _get_temperatures(number_of_particles):
     max_temperature = _get_max_temperature(number_of_particles)
     return [
@@ -180,12 +190,17 @@ def plot_specific_heat_capacity(
     plt.legend()
 
 
-def plot_critical_temperature(number_of_particles_list, ground_state_expected_values_list, temperature_range_list):
-    critical_temperate = [calculations.get_critical_temperature(
-        temperature_range=temperature_range_list[index],
-        ground_state_expected_values=ground_state_expected_values_list[index],
-        number_of_particles=number_of_particles_list[index])
-        for index in number_of_particles_list]
+def plot_critical_temperature(
+    number_of_particles_list, ground_state_expected_values_list, temperature_range_list
+):
+    critical_temperate = [
+        calculations.get_critical_temperature(
+            temperature_range=temperature_range_list[index],
+            ground_state_expected_values=ground_state_expected_values_list[index],
+            number_of_particles=number_of_particles_list[index],
+        )
+        for index in number_of_particles_list
+    ]
     plt.plot(number_of_particles_list, critical_temperate)
     plt.xlabel(r"$N$")
     plt.ylabel(r"$T_{C}\left(N\right)$")
